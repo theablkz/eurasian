ddc368bd285e836afd94a97ee3766c443c447a26
Abylay Baltabekov <theablkz@gmail.com>
2019-10-03 17:23:18 +0600

    finish?

--- static/api/v1/form.php
+++ static/api/v1/form.php
@@ -1,12 +1,6 @@
 <?php
-header('Access-Control-Allow-Origin:  *');
-header('Access-Control-Allow-Headers:  Content-Type, X-Auth-Token, Authorization, Origin, X-Requested-With, Accept');
-header('Access-Control-Allow-Methods:  GET , POST, PUT, OPTIONS, DELETE');
 $postData = file_get_contents('php://input');
 $data = json_decode($postData, true);
-
-
-
 
 $servername = "localhost";
 $username = "p-132_theablkz";
@@ -25,6 +19,47 @@ VALUES ('{$data['name']}','{$data['phone']}','{$data['iin']}','{$data['email']}'
 
 
 if ($conn->query($sql) === TRUE) {
+    $to      = 'smartlead.tm@gmail.com';
+    $subject = 'user registered';
+    $message = "
+    <html>
+    <head>
+      <title>Idengi новый клиент</title>
+    </head>
+    <body>
+    <table>
+
+        <tbody>
+        <tr>
+
+        <td>name</td>
+        <td>city</td>
+        <td>iin</td>
+        <td>email</td>
+        <td>phone</td>
+
+        </tr>
+        <tr>
+
+        <td>{$data['name']}</td>
+        <td>{$data['city']}</td>
+        <td>{$data['iin'] }</td>
+        <td>{$data['email']}</td>
+        <td>{$data['phone']}</td>
+
+        </tr>
+        </tbody>
+        </table>
+    </body>
+    </html>";
+    $headers[] = 'MIME-Version: 1.0';
+    $headers[] = 'Content-type: text/html; charset=iso-8859-1';
+    $headers[] = 'From: eubank@theablkz.kz';
+    $headers[] ='Reply-To: eubank@theablkz.kz';
+    $headers[] ='X-Mailer: PHP/' . phpversion();
+
+    mail($to, $subject, $message, implode("\r\n", $headers));
+
     echo "New record created successfully {$data['name']}";
 } else {
     echo "Error: " . $sql . "<br>" . $conn->error;
